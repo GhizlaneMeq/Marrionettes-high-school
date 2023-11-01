9814f022965ca6757c54f9444972de1f0fb6c3e8
nabilchabab <nabil.chababnabil@gmail.com>
2023-11-01 11:24:07 +0100

    Modified

--- assets/pages/dashboard/js/main.js
+++ assets/pages/dashboard/js/main.js
@@ -12,3 +12,74 @@ menu.onclick = () => {
     head.classList.toggle('left');
     header.classList.toggle('titleft');
 }
+
+const ctx = document.getElementById('Chart');
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' , 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      datasets: [{
+        label: '# Views',
+        data: [120, 1900, 300, 550, 220, 350 , 200 , 100 , 500 , 10 , 1000],
+        borderWidth: 3 ,
+        backgroundColor : 'blueViolet',
+        borderColor : 'blueViolet',
+        tension: 0.3,
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+const ctx1 = document.getElementById('lineChart');
+
+  new Chart(ctx1, {
+    type: 'line',
+    data: {
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' , 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      datasets: [{
+        label: '# Views',
+        data: [120, 1900, 300, 550, 220, 350 , 200 , 100 , 500 , 10 , 1000],
+        borderWidth: 3 ,
+        backgroundColor : 'blueViolet',
+        borderColor : 'blueViolet',
+        tension: 0.3,
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  const ctx2 = document.getElementById('doughnut');
+
+  new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'Red',
+            'Blue',
+            'Yellow'
+          ],
+          datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: [
+              'blueViolet',
+              'red',
+              'green'
+            ],
+            hoverOffset: 4
+          }]
+    },
+  });
